78bafaf4b9bde1244a881d3c8f8cbf049ab567bb
Athenkosi Magada <magadaathenkosi@gmail.com>
2024-01-15 14:09:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -222,6 +222,28 @@ function loadMoreHighlights() {
   } else {
     overMessage.style.display = 'none';
   }
+
+  const images = document.querySelectorAll('img');
+
+let imageOptions = {};
+
+let obsever = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) return;
+        const image = entry.target;
+        const newPath = image.getAttribute('data-src');
+        if(newPath) {
+            image.src = newPath;
+            observer.unobserve(image);
+        }
+    });
+}, imageOptions);
+
+images.forEach(image => {
+    if(image.getAttribute('data-src')) {
+        obsever.observe(image);
+    }
+});
 }
 
 // Initial load
@@ -290,6 +312,28 @@ function loadMoreInitiatives() {
   } else {
     overMessage2.style.display = 'none';
   }
+
+  const images = document.querySelectorAll('img');
+
+let imageOptions = {};
+
+let obsever = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) return;
+        const image = entry.target;
+        const newPath = image.getAttribute('data-src');
+        if(newPath) {
+            image.src = newPath;
+            observer.unobserve(image);
+        }
+    });
+}, imageOptions);
+
+images.forEach(image => {
+    if(image.getAttribute('data-src')) {
+        obsever.observe(image);
+    }
+});
 }
 
 // Initial load
